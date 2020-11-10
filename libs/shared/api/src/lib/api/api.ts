4647ad0f1a1ce2d0d3201ee78615149a7bfcad58
Yunus Kerem Turk <krmtrk20@gmail.com>
2020-11-10 16:40:18 +0300

    Add article api

--- libs/shared/api/src/lib/api/api.ts
+++ libs/shared/api/src/lib/api/api.ts
@@ -1,6 +1,8 @@
 import { AuthResource } from './Auth/auth-resource';
 import axios from '../config/axios';
+import { ArticleResource } from './Article/article-resource';
 
 export const api = {
   auth: new AuthResource(axios),
+  article: new ArticleResource(axios),
 };
--- libs/shared/api/src/lib/api/index.ts
+++ libs/shared/api/src/lib/api/index.ts
@@ -1,2 +1,3 @@
 export * from './api';
 export * from './Auth';
+export * from './Article';
