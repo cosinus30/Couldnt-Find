cd38ab3c41e8f4b335ae51406fbdb35f455a95c3
Kerem <krmtrk20@gmail.com>
2020-11-12 19:32:10 +0300

    Remove children from spinner

--- libs/ui/src/lib/atoms/Spinner/Spinner.tsx
+++ libs/ui/src/lib/atoms/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Spinner as RBSpinner } from 'react-bootstrap';
 
-export const Spinner = ({ children, ...props }) => {
-    return <RBSpinner animation="grow" variant="primary" className="my-auto" {...props}>{children}</RBSpinner>;
+export const Spinner = ({...props }) => {
+    return <RBSpinner animation="grow" variant="primary" className="my-auto" {...props}></RBSpinner>;
 };
