e713a7da6ed5ea3c0ad9427860b6650b7ed65ae3
Kerem <krmtrk20@gmail.com>
2020-11-12 11:24:12 +0300

    Add new endpoints

--- libs/shared/api/src/lib/api/Article/article-resource.ts
+++ libs/shared/api/src/lib/api/Article/article-resource.ts
@@ -3,23 +3,31 @@ import {
     CreateArticleRequest
 } from '@internship/shared/types';
 
-import { ArticleResponse } from './index'
+import { ArticleResponse, ArticlesResponse } from './index'
 
 export class ArticleResource {
     constructor(private axios: AxiosInstance = axiosStatic, private axiosRequestConfig: AxiosRequestConfig = {}) { }
 
     createArticle = (data: CreateArticleRequest): Promise<any> => this.axios.post("articles/", data, this.axiosRequestConfig).then((r) => r.data);
-    getTutorials = (): Promise<ArticleResponse[]> => {
+    getTutorials = (): Promise<ArticlesResponse[]> => {
         return this.axios.get('articles/tutorials', this.axiosRequestConfig).then((response) => { return response.data })
     };
-    getInsights = (): Promise<ArticleResponse[]> => {
+    getInsights = (): Promise<ArticlesResponse[]> => {
         return this.axios.get('articles/insights', this.axiosRequestConfig).then((response) => { return response.data })
     };
-    getEngineerings = (): Promise<ArticleResponse[]> => {
+    getEngineerings = (): Promise<ArticlesResponse[]> => {
         return this.axios.get('articles/engineerings', this.axiosRequestConfig).then((response) => { return response.data })
     };
 
     getTutorialById = (id: number): Promise<ArticleResponse> => this.axios.get("articles/tutorials/" + id, this.axiosRequestConfig).then((response) => { return response.data });
-    // updateArticle = ();
+    getInsightById = (id: number): Promise<ArticleResponse> => this.axios.get("articles/insights/" + id, this.axiosRequestConfig).then((response) => { return response.data });
+    getEngineeringById = (id: number): Promise<ArticleResponse> => this.axios.get("articles/engineerings/" + id, this.axiosRequestConfig).then((response) => { return response.data });
+
+    unlike = (id: number): Promise<any> => this.axios.post("articles/" + id + "/unlike", this.axiosRequestConfig).then((response) => { return response });
+    like = (id: number): Promise<any> => this.axios.post("articles/" + id + "/like", this.axiosRequestConfig).then((response) => { return response });
+
+    unbookmark = (id: number): Promise<any> => this.axios.post("articles/" + id + "/unbookmark", this.axiosRequestConfig).then((response) => { return response });
+    bookmark = (id: number): Promise<any> => this.axios.post("articles/" + id + "/bookmark", this.axiosRequestConfig).then((response) => { return response });
+
 
 }
--- libs/shared/api/src/lib/api/Article/types.ts
+++ libs/shared/api/src/lib/api/Article/types.ts
@@ -1,10 +1,36 @@
 export interface ArticleResponse {
+    article: Article;
+    liked: boolean;
+    bookmarked: boolean;
+}
+interface User {
+    email: string;
+    id: number;
+    name: string;
+    lastname: string;
+    username: string
+}
+
+interface Article {
     content: string;
     published: boolean;
     releaseDate: Date;
     contentType: string;
     readTime: number;
-    articleId: number;
+    id: number;
     heading: string;
-    author: string;
+    author: User;
+}
+
+export interface ArticlesResponse {
+    content: string;
+    published: boolean;
+    releaseDate: Date;
+    contentType: string;
+    readTime: number;
+    id: number;
+    heading: string;
+    author: User;
+    likeCount: number;
+    bookmarkCount: number;
 }
