d86c22af891565c650be3adc8c217af05d92e853
Kerem <krmtrk20@gmail.com>
2020-11-12 19:33:09 +0300

    Refactor api calls to singularize

--- libs/shared/api/src/lib/api/Article/article-resource.ts
+++ libs/shared/api/src/lib/api/Article/article-resource.ts
@@ -9,24 +9,15 @@ export class ArticleResource {
     constructor(private axios: AxiosInstance = axiosStatic, private axiosRequestConfig: AxiosRequestConfig = {}) { }
 
     createArticle = (data: CreateArticleRequest): Promise<any> => this.axios.post("articles/", data, this.axiosRequestConfig).then((r) => r.data);
-    getTutorials = (): Promise<ArticlesResponse[]> => {
-        return this.axios.get('articles/tutorials', this.axiosRequestConfig).then((response) => { return response.data })
-    };
-    getInsights = (): Promise<ArticlesResponse[]> => {
-        return this.axios.get('articles/insights', this.axiosRequestConfig).then((response) => { return response.data })
-    };
-    getEngineerings = (): Promise<ArticlesResponse[]> => {
-        return this.axios.get('articles/engineerings', this.axiosRequestConfig).then((response) => { return response.data })
-    };
 
     getArticles = (articleType: string): Promise<ArticlesResponse[]> => {
         return this.axios.get('articles/'+ articleType, this.axiosRequestConfig)
         .then((response) => { return response.data })
     };
 
-    getTutorialById = (id: number): Promise<ArticleResponse> => this.axios.get("articles/tutorials/" + id, this.axiosRequestConfig).then((response) => { return response.data });
-    getInsightById = (id: number): Promise<ArticleResponse> => this.axios.get("articles/insights/" + id, this.axiosRequestConfig).then((response) => { return response.data });
-    getEngineeringById = (id: number): Promise<ArticleResponse> => this.axios.get("articles/engineerings/" + id, this.axiosRequestConfig).then((response) => { return response.data });
+    getArticleById = (articleType: string, id: number) : Promise<ArticleResponse> => 
+        this.axios.get("articles/"+ articleType + "/" + id, this.axiosRequestConfig)
+        .then((response) => { return response.data });
 
     unlike = (id: number): Promise<any> => this.axios.post("articles/" + id + "/unlike", this.axiosRequestConfig).then((response) => { return response });
     like = (id: number): Promise<any> => this.axios.post("articles/" + id + "/like", this.axiosRequestConfig).then((response) => { return response });
