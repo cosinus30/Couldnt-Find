a714a98ed5c71316a335bec9ad4d7a44f14c0c92
Kerem <krmtrk20@gmail.com>
2020-11-13 18:07:55 +0300

    Add query params mapping

---
+++ libs/shared/types/src/lib/QueryParams.ts
@@ -0,0 +1,8 @@
+export const QueryParams = {
+    defaultSort: "sort=releaseDate,desc",
+    defaultSortWeek: "sort=releaseDate,desc&time=week",
+    defaultSortMonth: "sort=releaseDate,desc&time=month",
+    likeSort: "sort=likeCount,desc",
+    likeSortWeek: "sort=likeCount,desc&time=week",
+    likeSortMonth: "sort=likeCount,desc&time=month",
+}
--- libs/shared/types/src/lib/index.ts
+++ libs/shared/types/src/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './shared-types';
 export * from './article-request-types';
+export * from './QueryParams';
