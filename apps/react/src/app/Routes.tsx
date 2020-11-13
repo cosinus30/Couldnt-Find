7821267a9743408dc7038de08b5967f5497adf99
Kerem <krmtrk20@gmail.com>
2020-11-13 12:32:19 +0300

    Refactor route order

--- apps/react/src/app/Routes.tsx
+++ apps/react/src/app/Routes.tsx
@@ -18,21 +18,21 @@ export const Routes = ({ children, ...props }) => {
   return (
     <Router {...props}>
       {children}
-      <Route exact path="/" component={MainPage} />
       <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/write-your-story" component={WriteYourStory} />
+        <Route path="/forgotpassword" component={ForgotPasswordPage} />
+        <Route path="/resetpassword" component={ResetPassword} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/auth" component={OAuth2RedirectHandler} />
+        <Route exact path="/mailsuccess" component={MailSuccessPage} />
+        <Route exact path="/mailerror" component={MailErrorPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route path="/:articleType/:articleId" 
           component={() => <ArticleDetail {...props} key={window.location.pathname}/>} />
         <Route path="/:articleType" component={() => <Articles {...props} key={window.location.pathname}/>} />
       </Switch>
-      <Route path="/write-your-story" component={WriteYourStory} />
-      <Route path="/forgotpassword" component={ForgotPasswordPage} />
-      <Route path="/resetpassword" component={ResetPassword} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/auth" component={OAuth2RedirectHandler} />
-      <Route exact path="/mailsuccess" component={MailSuccessPage} />
-      <Route exact path="/mailerror" component={MailErrorPage} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
     </Router>
   );
 };
