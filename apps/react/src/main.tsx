6dfd742c8a8a8ac7004f9930be7b151bcf838373
Kerem <krmtrk20@gmail.com>
2020-11-13 13:36:10 +0300

    Refactor global styles

--- apps/react/src/main.tsx
+++ apps/react/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
 import { App } from './app/app';
 
 ReactDOM.render(
