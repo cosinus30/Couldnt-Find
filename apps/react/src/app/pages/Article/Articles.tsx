382f042577b1dff196264b13d1446d96ce90331a
Kerem <krmtrk20@gmail.com>
2020-11-18 13:58:41 +0300

    Refactor request to new params

--- apps/react/src/app/pages/Article/Articles.tsx
+++ apps/react/src/app/pages/Article/Articles.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Card, CardDeck, Col, Container, Pagination, Row, Spinner } from 'react-bootstrap';
 import { api, PageResponse } from '@internship/shared/api';
-import { BiChevronRight } from 'react-icons/bi';
 import { useHistory, useParams, Link} from 'react-router-dom';
-import { faBookmark, faEye, faHeart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useQuery } from '@internship/shared/hooks';
-import { Cards } from '@internship/ui';
+import { Cards, Dropdown } from '@internship/ui';
 import { QueryParams } from '@internship/shared/types';
 
 
@@ -14,30 +11,43 @@ export const Articles = () => {
   const history = useHistory();
   const {articleType} = useParams();
   const query = useQuery();
-  
+
+  console.log(articleType);
+
   const [page, setPage] = useState<PageResponse>();  
-  const [sessionInfo, setSessionInfo] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [sortType, setSortType] = useState(QueryParams.defaultSort);
+
+  // TODO Put these into global store
+  // ! Initial values are should be QueryParams["Most recent"] for sortType, QueryParams["3 days"] for span
+  const [sortType, setSortType] = useState(QueryParams["Most recent"]);
+  const [span, setSpan] = useState(QueryParams["3 days"]);
 
   useEffect(() => {
     api.article
-      .getArticles(articleType, query.get("page"), sortType, QueryParams.defaultSize)
+      .getArticles(articleType, query.get("page"), sortType, QueryParams.defaultSize, span)
       .then((response) => {
         console.log(response);
         setPage(response);
         setLoading(false);
       })
       .catch((e) => console.error(e));
-  }, [sessionInfo]);
+  }, [sortType, span]);
 
+  const sortHandler = (event) => {
+    setSortType(QueryParams[event.target.value]);
+  }
+
+  const spanHandler = (event) => {
+    setSpan(QueryParams[event.target.value])
+  }
 
   let items = [];
-
-
   let paginationBasic = null;
+  const sortTypes = ["Most recent","Most liked","Most popular"];
+  const sortDropdown = (<Dropdown  options={sortTypes} onSelectHandler={sortHandler}/>);
+  const spanTypes = ["3 days", "Week", "Month", "Infinity"];
+  const spanDropdown = (<Dropdown options={spanTypes} onSelectHandler={spanHandler}/>)
   
-
   let rendering = <Spinner animation="border"></Spinner>
   if (!loading) {
     for (let number = 1; number <= page.totalPages; number++) {
@@ -58,6 +68,12 @@ export const Articles = () => {
 
   return (
     <Container>
+      <Row>
+        <Col xs={12} md={{offset:9, span:3}}>
+          {sortDropdown}
+          {spanDropdown}
+        </Col>
+      </Row>
       <Row className="justify-content-center">
         {rendering}
       </Row>
