d94063842c3b72959766691241a180e370a20134
Kerem <krmtrk20@gmail.com>
2020-11-19 18:01:24 +0300

    Refactor query params to need

--- libs/shared/types/src/lib/QueryParams.ts
+++ libs/shared/types/src/lib/QueryParams.ts
@@ -1,11 +1,23 @@
 export const QueryParams = {
-    "Most recent": "&sort=releaseDate,desc",
-    "3 days": "&time=3days",
-    "Week": "&time=week",
-    "Month": "&time=month",
-    "Infinity": "&time=infinity",
-    "Most liked": "&sort=likeCount,desc",
-    "Most popular":"&sort=viewCount,desc",
+    "Most recent": "releaseDate,desc",
+    "3 days": "3days",
+    "Week": "week",
+    "Month": "month",
+    "Infinity": "infinity",
+    "Most liked": "likeCount,desc",
+    "Most popular":"viewCount,desc",
+    mainPageSize:"&size=3",
+    defaultSize:"&size=12",
+}
+
+export const QueryParamsReverse = {
+    "releaseDate,desc": "Most recent",
+    "3days": "3 days",
+    "week": "Week",
+    "month": "Month",
+    "infinity": "Infinity",
+    "likeCount,desc": "Most liked",
+    "viewCount,desc":"Most popular",
     mainPageSize:"&size=3",
     defaultSize:"&size=12",
 }
