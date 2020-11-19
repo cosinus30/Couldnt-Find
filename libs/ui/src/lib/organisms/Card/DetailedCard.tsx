b8fcb470b4aab7adf69ea2ab7b36e4ee48dc0354
Kerem <krmtrk20@gmail.com>
2020-11-19 17:59:43 +0300

    Refactor detailed card look

--- libs/ui/src/lib/organisms/Card/DetailedCard.tsx
+++ libs/ui/src/lib/organisms/Card/DetailedCard.tsx
@@ -1,7 +1,12 @@
 import { ArticleResponse } from '@internship/shared/api';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import { IconButtonRow, IconButtonRowProps } from '../../molecules';
+import classes from './DetailedCard.module.css'
+import mySvg from '../../../../../../apps/react/src/assets/girl-1.svg';
+import { Months } from '@internship/shared/types';
+import { faBookmark, faEye, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type DetailCardProps = {
   articleDetail: ArticleResponse;
@@ -9,20 +14,63 @@ type DetailCardProps = {
 };
 
 export const DetailedCard: React.FC<DetailCardProps> = ({ children, ...props }) => {
+  const relDate = new Date(props.articleDetail.article.releaseDate); 
+
+  var day = relDate.getDate(),
+  month = relDate.getMonth(),
+  year = relDate.getFullYear();
+
+
   return (
     <Card bg="secondary" className="mx-auto my-5" text="light">
-      <Card.Body>
-        <IconButtonRow {...props.userInteraction}></IconButtonRow>
+      <Card.Body className="pt-0">
+        <Card.Header >
+          <Row>
+            <Col xs="4">
+              <Row>
+                <Col xs="4" className="pr-1">
+                  <img src={mySvg} height="50px" width="50px" alt="React Logo" />
+                </Col>
+                <Col xs="8" className="pl-0">
+                  <Row>
+                    <Col xs="12">
+                      {props.articleDetail.article.author.username}
+                    </Col>
+                    <Col xs="12" >
+                      <small>{ day + " " + Months[month+1] + " " + year}</small>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs="8">
+              <IconButtonRow {...props.userInteraction}></IconButtonRow>
+            </Col>
+          </Row>
+        </Card.Header>
         <Card.Title>{props.articleDetail.article.heading}</Card.Title>
-        <Card.Text>
-          {props.articleDetail.article.readTime} min read
-      </Card.Text>
-        <Card.Text>
-          {props.articleDetail.article.content}
-        </Card.Text>
+        <div>
+          <div className={classes.pre} dangerouslySetInnerHTML={{__html : props.articleDetail.article.content}}></div>
+        </div>
       </Card.Body>
       <Card.Footer>
-        <p>{props.articleDetail.article.releaseDate}</p>
+        <Row>
+          <Col xs={2}>
+            <small className="float-left">
+              <FontAwesomeIcon icon={faHeart}/><span className="text-muted"> 3 likes</span> 
+            </small>
+          </Col>
+          <Col xs={2}>
+            <small className="float-left">
+              <FontAwesomeIcon icon={faEye}/><span className="text-muted"> 4 views</span> 
+            </small>
+          </Col>
+          <Col xs={2}>
+            <small className="float-left">
+              <FontAwesomeIcon icon={faBookmark}/><span className="text-muted"> 5 bookmarks</span> 
+            </small>
+          </Col>
+        </Row>
       </Card.Footer>
     </Card>
   );
