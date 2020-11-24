a0cd4976854d0c6adfde360334be9372d706fd6a
Kerem <krmtrk20@gmail.com>
2020-11-24 13:35:53 +0300

    Refactor styling of cards

--- libs/ui/src/lib/organisms/Card/Cards.tsx
+++ libs/ui/src/lib/organisms/Card/Cards.tsx
@@ -1,11 +1,14 @@
 import { faBookmark, faEye, faHeart } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  PageResponse } from '@internship/shared/api';
+import { Months } from '@internship/shared/types';
 import React from 'react';
 import { Card as RBCard, Col, Row } from 'react-bootstrap';
 import {useHistory} from 'react-router-dom';
-import code from './codecode.jpg';
 import styled from 'styled-components';
+import mySvg from '../../../../../../apps/react/src/assets/girl-1.svg';
+import classes from './Cards.module.css';
+
 
 type CardProps = {
   page: PageResponse;
@@ -55,44 +58,77 @@ export const Cards: React.FC<CardProps> = ({ children, ...props }) => {
       </Col>
     </Row>
     <Row>
-    {    props.page.content?.map((article) => (
-      <Col key={article.id} md="6" xs="12" lg="4">
-        <StyledRBCard className="m-3 bg-mine text-light"  onClick={() => goToLinkHandler(article.id)}>
-          <RBCard.Img style={{maxHeight: "25vh", objectFit: "cover" , aspectRatio: "3/2"}} variant="top" src={code} />
-          <RBCard.Body>
+    {    props.page.content?.map((article) => {
+        const relDate = new Date(article.releaseDate); 
+        var day = relDate.getDate(),
+        month = relDate.getMonth(),
+        year = relDate.getFullYear();
+      return (<Col key={article.id} md="6" xs="12" lg="4">
+        <StyledRBCard className={classes.fancy_card + " m-3 bg-mine text-light"}>
+          <RBCard.Header>
+            <Row>
+              <Col xs="8">
+                <a>
+                  <Row>
+                    <Col xs="4" className="pr-1">
+                      <img src={mySvg} height="50px" width="100%" alt="React Logo" />
+                    </Col>
+                    <Col xs="8" className="pl-1">
+                      <Row>
+                        <Col xs="12">
+                          {article.author.username}
+                        </Col>
+                        <Col xs="12" >
+                          <small>{ day + " " + Months[month+1] + " " + year}</small>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </a>
+              </Col>
+            </Row>
+          </RBCard.Header>
+          <RBCard.Img onClick={() => goToLinkHandler(article.id)} 
+          style={{maxHeight: "17.5vh", objectFit: "cover" , aspectRatio: "3/2"}} 
+          variant="top" 
+          src='https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80' />
+          <RBCard.Body className={classes.CardBody}>
+            <div className="my-2">
+            {article.tags.map((tag) => {
+              return (<div className={classes.tag} key={tag.tagName}>#{tag.tagName}</div>)
+            })}
+            </div>
+            
             <RBCard.Title>{article.heading}</RBCard.Title>
-            <div style={{height:150, overflowY: 'hidden',width:'300px' , color: '#E9D7DA !important'}} >
+            <div style={{height:100, overflowY: 'hidden',maxWidth:'350px' , color: '#E9D7DA !important'}} >
               <div style={{color: '#E9D7DA !important'}} dangerouslySetInnerHTML={{__html : article.content}}></div>
             </div>
           </RBCard.Body>
           <hr className="bg-primary my-1"/>
             <Row className="mx-1 my-1">
               <Col>
-                <Row>
-                  <Col xs={12}>
+                <Row className="my-2">
+                  <Col xs={4}>
                     <small className="float-left">
-                      <FontAwesomeIcon icon={faHeart}/><span className="text-muted"> {article.likeCount} likes</span> 
+                      <FontAwesomeIcon icon={faHeart}/><span className="text-muted"> {article.likeCount}</span> 
                     </small>
                   </Col>
-                  <Col xs={12}>
+                  <Col xs={4}>
                     <small className="float-left">
-                      <FontAwesomeIcon icon={faEye}/><span className="text-muted"> {article.viewCount} views</span> 
+                      <FontAwesomeIcon icon={faEye}/><span className="text-muted"> {article.viewCount}</span> 
                     </small>
                   </Col>
-                  <Col xs={12}>
+                  <Col xs={4}>
                     <small className="float-left">
-                      <FontAwesomeIcon icon={faBookmark}/><span className="text-muted"> {article.bookmarkCount} bookmarks</span> 
+                      <FontAwesomeIcon icon={faBookmark}/><span className="text-muted"> {article.bookmarkCount}</span> 
                     </small>
                   </Col>
                 </Row>
               </Col>
-              <Col className="my-auto">
-              <small className="text-muted float-right">Written by {article.author.username}</small>
-              </Col>
             </Row>
         </StyledRBCard>
       </Col>
-    ))}
+    )})}
 
     </Row>
     </React.Fragment>
